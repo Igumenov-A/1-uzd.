be9abf6b0150cad7298b4b2ac7abc2841a89bdc2
Igumenov-A <60686865+Igumenov-A@users.noreply.github.com>
2020-02-07 11:28:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,33 @@
 #include <string>
 
 struct vizitine{
-    std::string var, eil1="*", eil2="*", eil3="* ";
-
+    std::string var, eil1="*", eil2="*", eil3="* ", eil4;
+    int eil_sk, son_tarpai;
     void nusk(){
         std::cout << "Vardas?" << std::endl;
         std::cin>>var;
-        eil3+="Sveikas, "+var+"! *\n";
+        std::cout<<"Iveskite remelio eil. sk.:"; std::cin>>eil_sk;
+        std::cout<<"Iveskite remelio son. tarpu sk.:"; std::cin>>son_tarpai;
+        for (int i=0; i<son_tarpai; i++, eil3+=" ");
+        if (*var.rbegin() == 's') eil4="Sveikas, "; else eil4="Sveika, ";
+        eil3+=eil4+var+"!";
+
+        for (int i=0; i<son_tarpai; i++, eil3+=" ");
+
+        eil3+="*\n";
         int n=eil3.length();
-        for (int i = 1; i < n - 2; i++, eil1= eil1 + "*", eil2= eil2 + " ");
-        eil1=eil1+"*\n";eil2=eil2+"*\n";
-    }
+
+        for (auto i=1; i < n - 2; i++, eil1= eil1 + "*", eil2= eil2 + " ");
+
+        eil1=eil1+"*\n";
+        eil2=eil2+"*\n";
+        }
     void spausd(){
-        std::cout<<eil1<<eil2<<eil3<<eil2<<eil1;
+        std::cout<<eil1;
+        for (int i=0; i<eil_sk;i++, std::cout<<eil2);
+        std::cout<<eil3;
+        for (int i=0; i<eil_sk;i++, std::cout<<eil2);
+        std::cout<<eil1;
     }
 };
 
@@ -22,6 +37,7 @@ int main()
     vizitine V1;
     V1.nusk();
     V1.spausd();
-    std::cout<<"Pabaiga";
+    std::cout<<"Programos pabaiga";
+    std::cin.get();
     return 0;
 }
