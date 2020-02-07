644284e730a52d206f4cede32057de2f066f4a4c
Igumenov-A <60686865+Igumenov-A@users.noreply.github.com>
2020-02-07 11:02:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <string>
+
+struct vizitine{
+    std::string var, eil1="*", eil2="*", eil3="* ";
+
+    void nusk(){
+        std::cout << "Vardas?" << std::endl;
+        std::cin>>var;
+        eil3+="Sveikas, "+var+"! *\n";
+        int n=eil3.length();
+        for (int i = 1; i < n - 2; i++, eil1= eil1 + "*", eil2= eil2 + " ");
+        eil1=eil1+"*\n";eil2=eil2+"*\n";
+    }
+    void spausd(){
+        std::cout<<eil1<<eil2<<eil3<<eil2<<eil1;
+    }
+};
 
 int main()
 {
-        std::string var, eil1="*", eil2="*", eil3="* ", eil4;
-
-        std::cout << "Vardas?" << std::endl;
-        std::cin>>var;
-        if (*var.rbegin() == 's') eil4="Sveikas, "; else eil4="Sveika, ";
-        eil3+=eil4+var+"! *\n";
-        int n=eil3.length();
-
-        for (auto i=1; i < n - 2; i++, eil1= eil1 + "*", eil2= eil2 + " ");
-
-        eil1=eil1+"*\n";eil2=eil2+"*\n";
-
-        std::cout<<eil1<<eil2<<eil3<<eil2<<eil1;
-    std::cout<<"Programos pabaiga";
-    std::cin.get();
+    vizitine V1;
+    V1.nusk();
+    V1.spausd();
+    std::cout<<"Pabaiga";
     return 0;
 }
